ba7782180e9941d6ca68550dfad095bc8d15f656
Bert De Saffel <bertdesaffel@gmail.com>
2018-10-15 23:31:00 +0200

    hernieuwing

--- Besturingssystemen III/Labo/Reeks 4/26.pl
+++ Besturingssystemen III/Labo/Reeks 4/26.pl
@@ -6,3 +6,4 @@ use Win32::OLE qw(in);
 my $locator = Win32::OLE->new("WbemScripting.SWbemLocator");
 my $service = $locator->ConnectServer("127.0.0.1", "root/CIMV2");
 
+
--- Gevorderde algoritmen/Samenvatting/dot2png.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-	for /R "E:\Master Informatica\Gevorderde algoritmen\Samenvatting\img" %%f in (*.dot) do (
-	dot img\%%~nf%%~xf -Tpng -o img\%%~nf.png
-)
