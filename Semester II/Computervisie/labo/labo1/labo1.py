805bb367fd45da4987bd5a0bb258282ae472dd2b
monoscale <bertdesaffel@gmail.com>
2019-02-16 15:01:55 +0100

    computervisie labo 1

--- Semester II/Computervisie/labo/labo1/labo1.py
+++ Semester II/Computervisie/labo/labo1/labo1.py
@@ -5,36 +5,35 @@ import os.path
 try:
     filePath = sys.argv[1]
 except IndexError:
-    print("One parameter requested: the path of an image.")
+    print("One parameter required: the path of an image.")
     exit(1)
 
 if (not os.path.isfile(filePath)):
     print("'" + filePath + "' is not an existing file.")
     exit(2)
 
-
-
 path, extension = filePath.split(".") # seperate path and extension:  [/path/to/file].[extension]
-# these variables are used later on to write the grayscale and threshold images, but also to create a relevant window name
-
-windowName = (path.split("/"))[-1] # access last element when path is splitted on /, so we get the filename, without the extension: /path/to/[file]
-cv2.namedWindow(windowName)
+cv2.namedWindow(path)
 
 # DEFAULT
 defaultImage = cv2.imread(filePath)
-cv2.imshow(windowName, defaultImage)
+print(defaultImage.data)
+cv2.imshow(path, defaultImage)
 cv2.waitKey()
 
 # GRAYSCALE
+print("Converting to grayscale")
 grayScaleImage = cv2.cvtColor(defaultImage, cv2.COLOR_BGR2GRAY);
-cv2.imshow(windowName, grayScaleImage)
+cv2.imshow(path, grayScaleImage)
 cv2.waitKey()
 
 # THRESHOLD
+print("Thresholding the grayscale image at 50% of the maximum intensity")
 ret, thresholdImage = cv2.threshold(grayScaleImage, 127, 255, cv2.THRESH_BINARY)
-cv2.imshow(windowName, thresholdImage)
+cv2.imshow(path, thresholdImage)
 cv2.waitKey()
 
 # WRITING
+print("Saving grayscale and threshold images")
 cv2.imwrite(path + "GRAYSCALE." + extension, grayScaleImage)
 cv2.imwrite(path + "THRESHOLD." + extension, thresholdImage)
