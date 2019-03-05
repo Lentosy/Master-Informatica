2d46b908e2f34a936263d7d84da175e1d56ef607
monoscale <bertdesaffel@gmail.com>
2019-03-05 15:11:32 +0100

    huh

--- Semester II/Computervisie/labo/labo4/labo4_ex9.py
+++ Semester II/Computervisie/labo/labo4/labo4_ex9.py
@@ -3,33 +3,48 @@ import sys
 import os.path
 import numpy
 
-#shadow.png
+#shadow_box.png
 try:
     fullPath = sys.argv[1]
 except IndexError:
-    print("One parameter required: the path of an image.")
+    print("One parameter required: the path of an image. (shadow_box.jpg)")
     exit(-1)
 
 if (not os.path.isfile(fullPath)):
     print("'" + fullPath + "' is not an existing file.")
     exit(-1)
 
+
 filepath, filename = os.path.split(fullPath)
 cv2.namedWindow(filename)
 image = cv2.imread(fullPath)
 cv2.imshow(filename, image)
 
-#https://docs.opencv.org/4.0.1/da/d54/group__imgproc__transform.html#ga20f62aa3235d869c9956436c870893ae
 
-#https://docs.opencv.org/4.0.1/da/d54/group__imgproc__transform.html#gaf73673a7e8e18ec6963e3774e6a94b87
-def warp(event, x, y, flags, userdata):
-    if(cv2.EVENT_LBUTTONUP == event):
-        print("Clicked (" + str(x) + ", " + str(y) + ") [ " + str(flags) + " ]")
+src = numpy.array([[0, 0], [0, 0], [0, 0], [0, 0]], numpy.float32) 
+dst = numpy.array([[100, 200], [200, 200], [200, 500], [100, 500]], numpy.float32) # predefined coördinates of the rectangle
 
-cv2.setMouseCallback(filename,warp)
+def onMouse(event, x, y, flags, userdata):
+    global mouseClicks, image, filename
+    if(cv2.EVENT_LBUTTONDOWN == event and mouseClicks < 4):  
+        src[mouseClicks] = [x, y]
+        mouseClicks += 1
+    if(mouseClicks == 4):
+        mouseClicks += 1 # increase one more time so this function will basically do nothing anymore
+        
+        M = cv2.getPerspectiveTransform(src, dst) # Get the transformation matrix
+        warped = cv2.warpPerspective(image, M, (-1, -1)) # warp the image
+        result = cv2.hconcat((image, warped)) # concatenate the original image and warped image
+        cv2.imshow(filename, result) # show original and warped image
+
+        # Saving the warped image
+        filename, extension = filename.split(".")
+        savePath = filepath + "\\" + filename + "EX9." + extension
+        print("Saving as: " + savePath)
+        cv2.imwrite(savePath, warped)
+
+
+mouseClicks = 0 
+cv2.setMouseCallback(filename, onMouse, mouseClicks)
 cv2.waitKey()
 
-filename, extension = filename.split(".")
-savePath = filepath + "\\" + filename + "EX8." + extension
-print("Saving as: " + savePath)
-cv2.imwrite(savePath, image)
