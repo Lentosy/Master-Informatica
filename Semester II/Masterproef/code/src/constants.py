365fb4a57a6455720cf43492a16acbc8bd4f3aaf
monoscale <bertdesaffel@gmail.com>
2019-04-01 11:46:31 +0200

    labeling

--- Semester II/Masterproef/code/src/constants.py
+++ Semester II/Masterproef/code/src/constants.py
@@ -1,5 +1,5 @@
-from pykinect2 import PyKinectV2
-import pygame
+#from pykinect2 import PyKinectV2
+#import pygame
 
 PERSONS = ['BERT', 'DEMING', 'NIELS']
 ACTIONS = ['DEFAULT',               'EMERGENCY_STOP',       
@@ -15,61 +15,62 @@ DEFAULT_FPS = 30
 SKELETON_LINE_THICKNESS = 28
 SKELETON_CIRCLE_RADIUS = 25
 
-SKELETON_COLORS = [
-    pygame.color.THECOLORS['red'], 
-    pygame.color.THECOLORS['blue'], 
-    pygame.color.THECOLORS['green'], 
-    pygame.color.THECOLORS['orange'], 
-    pygame.color.THECOLORS['purple'], 
-    pygame.color.THECOLORS['yellow']
-]
-
-# List of all the available joints
-JOINTS = [
-    PyKinectV2.JointType_Head,          PyKinectV2.JointType_Neck, 
-    PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_SpineMid,      PyKinectV2.JointType_SpineBase, 
-    PyKinectV2.JointType_ShoulderLeft,  PyKinectV2.JointType_ShoulderRight, 
-    PyKinectV2.JointType_HipRight,      PyKinectV2.JointType_HipLeft,
-    PyKinectV2.JointType_ElbowRight,    PyKinectV2.JointType_ElbowLeft, 
-    PyKinectV2.JointType_WristLeft,     PyKinectV2.JointType_WristRight,
-    PyKinectV2.JointType_HandLeft,      PyKinectV2.JointType_HandRight,
-    PyKinectV2.JointType_ThumbLeft,     PyKinectV2.JointType_ThumbRight,
-    PyKinectV2.JointType_AnkleLeft,     PyKinectV2.JointType_AnkleRight,
-    PyKinectV2.JointType_HandTipLeft,   PyKinectV2.JointType_HandTipRight,
-    PyKinectV2.JointType_KneeLeft,      PyKinectV2.JointType_KneeRight,
-    PyKinectV2.JointType_FootLeft,      PyKinectV2.JointType_FootRight
-
-]
-
-# List of all the connections between two joints
-CONNECTIONS = [
-    # Torso
-    [PyKinectV2.JointType_Head,             PyKinectV2.JointType_Neck],
-    [PyKinectV2.JointType_Neck,             PyKinectV2.JointType_SpineShoulder],
-    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_SpineMid],
-    [PyKinectV2.JointType_SpineMid,         PyKinectV2.JointType_SpineBase],
-    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_ShoulderRight],
-    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_ShoulderLeft],
-    [PyKinectV2.JointType_SpineBase,        PyKinectV2.JointType_HipRight],
-    [PyKinectV2.JointType_SpineBase,        PyKinectV2.JointType_HipLeft],
-    # Right arm
-    [PyKinectV2.JointType_ShoulderRight,    PyKinectV2.JointType_ElbowRight],
-    [PyKinectV2.JointType_ElbowRight,       PyKinectV2.JointType_WristRight],
-    [PyKinectV2.JointType_WristRight,       PyKinectV2.JointType_HandRight],
-    [PyKinectV2.JointType_HandRight,        PyKinectV2.JointType_HandTipRight],
-    [PyKinectV2.JointType_WristRight,       PyKinectV2.JointType_ThumbRight],
-    # Left arm
-    [PyKinectV2.JointType_ShoulderLeft,     PyKinectV2.JointType_ElbowLeft],
-    [PyKinectV2.JointType_ElbowLeft,        PyKinectV2.JointType_WristLeft],
-    [PyKinectV2.JointType_WristLeft,        PyKinectV2.JointType_HandLeft],
-    [PyKinectV2.JointType_HandLeft,         PyKinectV2.JointType_HandTipLeft],
-    [PyKinectV2.JointType_WristLeft,        PyKinectV2.JointType_ThumbLeft],
-    # Right leg
-    [PyKinectV2.JointType_HipRight,         PyKinectV2.JointType_KneeRight],
-    [PyKinectV2.JointType_KneeRight,        PyKinectV2.JointType_AnkleRight],
-    [PyKinectV2.JointType_AnkleRight,       PyKinectV2.JointType_FootRight],
-    # Left leg
-    [PyKinectV2.JointType_HipLeft,          PyKinectV2.JointType_KneeLeft],
-    [PyKinectV2.JointType_KneeLeft,         PyKinectV2.JointType_AnkleLeft],
-    [PyKinectV2.JointType_AnkleLeft,        PyKinectV2.JointType_FootLeft]
-]
+#SKELETON_COLORS = [
+#    pygame.color.THECOLORS['red'], 
+#    pygame.color.THECOLORS['blue'], 
+#    pygame.color.THECOLORS['green'], 
+#    pygame.color.THECOLORS['orange'], 
+#    pygame.color.THECOLORS['purple'], 
+#    pygame.color.THECOLORS['yellow']
+#]
+#
+## List of all the available joints
+#JOINTS = [
+#    PyKinectV2.JointType_Head,          PyKinectV2.JointType_Neck, 
+#    PyKinectV2.JointType_SpineShoulder, PyKinectV2.JointType_SpineMid,      PyKinectV2.JointType_SpineBase, 
+#    PyKinectV2.JointType_ShoulderLeft,  PyKinectV2.JointType_ShoulderRight, 
+#    PyKinectV2.JointType_HipRight,      PyKinectV2.JointType_HipLeft,
+#    PyKinectV2.JointType_ElbowRight,    PyKinectV2.JointType_ElbowLeft, 
+#    PyKinectV2.JointType_WristLeft,     PyKinectV2.JointType_WristRight,
+#    PyKinectV2.JointType_HandLeft,      PyKinectV2.JointType_HandRight,
+#    PyKinectV2.JointType_ThumbLeft,     PyKinectV2.JointType_ThumbRight,
+#    PyKinectV2.JointType_AnkleLeft,     PyKinectV2.JointType_AnkleRight,
+#    PyKinectV2.JointType_HandTipLeft,   PyKinectV2.JointType_HandTipRight,
+#    PyKinectV2.JointType_KneeLeft,      PyKinectV2.JointType_KneeRight,
+#    PyKinectV2.JointType_FootLeft,      PyKinectV2.JointType_FootRight
+#
+#]
+#
+## List of all the connections between two joints
+#CONNECTIONS = [
+#    # Torso
+#    [PyKinectV2.JointType_Head,             PyKinectV2.JointType_Neck],
+#    [PyKinectV2.JointType_Neck,             PyKinectV2.JointType_SpineShoulder],
+#    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_SpineMid],
+#    [PyKinectV2.JointType_SpineMid,         PyKinectV2.JointType_SpineBase],
+#    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_ShoulderRight],
+#    [PyKinectV2.JointType_SpineShoulder,    PyKinectV2.JointType_ShoulderLeft],
+#    [PyKinectV2.JointType_SpineBase,        PyKinectV2.JointType_HipRight],
+#    [PyKinectV2.JointType_SpineBase,        PyKinectV2.JointType_HipLeft],
+#    # Right arm
+#    [PyKinectV2.JointType_ShoulderRight,    PyKinectV2.JointType_ElbowRight],
+#    [PyKinectV2.JointType_ElbowRight,       PyKinectV2.JointType_WristRight],
+#    [PyKinectV2.JointType_WristRight,       PyKinectV2.JointType_HandRight],
+#    [PyKinectV2.JointType_HandRight,        PyKinectV2.JointType_HandTipRight],
+#    [PyKinectV2.JointType_WristRight,       PyKinectV2.JointType_ThumbRight],
+#    # Left arm
+#    [PyKinectV2.JointType_ShoulderLeft,     PyKinectV2.JointType_ElbowLeft],
+#    [PyKinectV2.JointType_ElbowLeft,        PyKinectV2.JointType_WristLeft],
+#    [PyKinectV2.JointType_WristLeft,        PyKinectV2.JointType_HandLeft],
+#    [PyKinectV2.JointType_HandLeft,         PyKinectV2.JointType_HandTipLeft],
+#    [PyKinectV2.JointType_WristLeft,        PyKinectV2.JointType_ThumbLeft],
+#    # Right leg
+#    [PyKinectV2.JointType_HipRight,         PyKinectV2.JointType_KneeRight],
+#    [PyKinectV2.JointType_KneeRight,        PyKinectV2.JointType_AnkleRight],
+#    [PyKinectV2.JointType_AnkleRight,       PyKinectV2.JointType_FootRight],
+#    # Left leg
+#    [PyKinectV2.JointType_HipLeft,          PyKinectV2.JointType_KneeLeft],
+#    [PyKinectV2.JointType_KneeLeft,         PyKinectV2.JointType_AnkleLeft],
+#    [PyKinectV2.JointType_AnkleLeft,        PyKinectV2.JointType_FootLeft]
+#]
+#
--- Semester II/Masterproef/code/src/pipeline.py
+++ Semester II/Masterproef/code/src/pipeline.py
@@ -20,44 +20,45 @@ class Dataset(object):
         self.data = []
         self.target = []
 
-def load_trainingset():
-    trainingset = Dataset() # trainingset is a n * k matrix with n = # samples (frames) and k = # features (175)
-    for i in range(0, len(ACTIONS)):
-        for j in range(1, 10):
-            try:
-                df = pd.read_csv(FOLDER + f"{PERSONS[0]}_{ACTIONS[i]}_{j}.txt", header = None, sep = ';')
-                trainingset.target.extend(df.iloc[:][175].values)
-                trainingset.data.extend(df.iloc[:][range(0, 175)].values)
-            except FileNotFoundError as fnfe:
-                print(fnfe)
-    return trainingset
-
-def load_validationset():
-    validationset = Dataset()
-    print(len(validationset.data))
-    for i in range(0, len(ACTIONS)):
-        try:
-            df = pd.read_csv(FOLDER + f"{PERSONS[0]}_{ACTIONS[i]}_{10}.txt", header = None, sep = ';')
-            validationset.target.extend(df.iloc[:][175].values)
-            validationset.data.extend(df.iloc[:][range(0, 175)].values)
-        except FileNotFoundError as fnfe:
-            print(fnfe)
-
-    return validationset
-
-
-
-trainingset = load_trainingset()
-validationset = load_validationset()
-
-for name, clf in zip(names, classifiers):
-    clf.fit(trainingset.data, trainingset.target)
-    res = clf.predict(validationset.data)
-    positive, negative = (0, 0)
-    for i in range(0, len(res)):
-        if(res[i] == validationset.target[i]):
-            positive += 1
-        else:
-            negative += 1
-        
-    print(f"[{name}]: correct = {positive/len(res) * 100}%, false = {negative/len(res) * 100}%")
+#def load_trainingset():
+#    trainingset = Dataset() # trainingset is a n * k matrix with n = # samples (frames) and k = # features (175)
+#    for i in range(1, len(ACTIONS)):
+#        try:
+#            joints = pd.read_csv(FOLDER + f"{PERSONS[0]}\\{ACTIONS[i]}\\joints.txt", header = None, sep = ';')
+#            labels = pd.read_csv(FOLDER + f"{PERSONS[0]}\\{ACTIONS[i]}\\labels.txt")
+#                
+#            trainingset.target.extend(labels.values)
+#            trainingset.data.extend(joints.values)
+#        except FileNotFoundError as fnfe:
+#            print(fnfe)
+#    return trainingset
+#
+#def load_validationset():
+#    validationset = Dataset()
+#    try:
+#        joints = pd.read_csv(FOLDER + f"{PERSONS[0]}\\{ACTIONS[len(ACTIONS) - 2]}\\joints.txt", header = None, sep = ';')
+#        labels = pd.read_csv(FOLDER + f"{PERSONS[0]}\\{ACTIONS[len(ACTIONS) - 2]}\\labels.txt")
+#        validationset.target.extend(labels.values)
+#        validationset.data.extend(joints.values)
+#    except FileNotFoundError as fnfe:
+#        print(fnfe)
+#
+#    return validationset
+#
+#
+#
+#trainingset = load_trainingset()
+#validationset = load_validationset()
+#
+#for name, clf in zip(names, classifiers):
+#    clf.fit(trainingset.data, trainingset.target)
+#    res = clf.predict(validationset.data)
+#    positive, negative = (0, 0)
+#    for i in range(0, len(res)):
+#        if(res[i] == validationset.target[i]):
+#            positive += 1
+#        else:
+#            negative += 1
+#        
+#    print(f"[{name}]: correct = {positive/len(res) * 100}%, false = {negative/len(res) * 100}%")
+#
