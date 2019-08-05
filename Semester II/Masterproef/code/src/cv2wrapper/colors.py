90c4370f4408914d7ad4ec8b4f4d5f82064d01fd
unknown <monoscale@outlook.com>
2019-08-05 21:39:09 +0200

    deathblow

--- Semester II/Masterproef/code/src/cv2wrapper/colors.py
+++
@@ -1,7 +0,0 @@
-
-# Colors are BGR tuples
-GREEN = (0,255,0)
-RED = (0,0,255)
-BLUE = (255,0,0)
-CYAN = (255, 255, 0)
-ORANGE = (0, 165, 255)
--- Semester II/Masterproef/code/src/pip_install.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-packages = ['pykinect2', 'matplotlib', 'pygame', 'pandas' , 'numpy', 'sklearn', 'pyquaternion']
-for package in packages:
-    subprocess.call([sys.executable, '-m' , 'pip', 'install', package])
