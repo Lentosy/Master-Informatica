24c3cf04aead777831612082f724398a7ff6d4da
unknown <monoscale@outlook.com>
2019-10-15 20:07:02 +0200

    voorlopige methode opstelling boom

--- Semester I/Gevorderde algoritmen/Labo 2019-2020/3a. Eieren/eieren.h
+++ Semester I/Gevorderde algoritmen/Labo 2019-2020/3a. Eieren/eieren.h
@@ -32,31 +32,33 @@ void printMatrix(std::vector<std::vector<int>>& matrix){
 
 class Eieren {
     public:
-        Eieren(int e, int v) : eGezocht{e}, vGezocht{v} {};
+        Eieren(int e, int v) : eGezocht{e}, vGezocht{v}, kostTabel{this->vGezocht + 1, std::vector<int>(this->eGezocht + 1)}, wortelTabel{this->vGezocht + 1, std::vector<int>(this->eGezocht + 1)} {
+            berekenTabellen();
+        };
         int geefGoedkoopsteOplossing() const;
+        Boom<int, int> geefBoom() const;
     private:
         int eGezocht; // aantal eieren
         int vGezocht; // aantal verdiepingen
+        std::vector<std::vector<int>> kostTabel;
+        std::vector<std::vector<int>> wortelTabel;
+        void berekenTabellen();
+        void maakDeelbomen(Boom<int, int>& wortel, int huidigAantalEieren, int aantalVerdiepingenLinks, int aantalVerdiepingenRechts) const;
 };
 
-int Eieren::geefGoedkoopsteOplossing() const {
-    std::vector<std::vector<int>> kostTabel(this->vGezocht + 1, std::vector<int>(this->eGezocht + 1));
-    std::vector<std::vector<int>> wortelTabel(this->vGezocht + 1, std::vector<int>(this->eGezocht + 1));
-    
-
-
-    // Basisgeval: voor verdieping 0 zijn er geen pogingen nodig, voor verdieping 1 is er 1 poging nodig.
+void Eieren::berekenTabellen() {
+  // Basisgeval: voor verdieping 0 zijn er geen pogingen nodig, voor verdieping 1 is er 1 poging nodig.
     for(int e = 1; e <= this->eGezocht; e++){
-        kostTabel[0][e] = 0;
-        kostTabel[1][e] = 1;
-        wortelTabel[0][e] = 0;
-        wortelTabel[1][e] = 1;
+        this->kostTabel[0][e] = 0;
+        this->kostTabel[1][e] = 1;
+        this->wortelTabel[0][e] = 0;
+        this->wortelTabel[1][e] = 1;
     }
     
     // Basisgeval: bij 1 ei is het aantal pogingen gelijk aan het aantal verdiepingen
     for(int v = 1; v <= this->vGezocht; v++){
-        kostTabel[v][1] = v;
-        wortelTabel[v][1] = v;
+        this->kostTabel[v][1] = v;
+        this->wortelTabel[v][1] = v;
     }
 
     
@@ -67,28 +69,64 @@ int Eieren::geefGoedkoopsteOplossing() const {
             for(int w = 1; w <= v; w++){
                 int diepteGebroken = kostTabel[w - 1][e - 1]; // Beste diepte van de linkerdeelboom
                 int diepteNietGebroken = kostTabel[v - w][e]; // Beste diepte van de rechterdeelboom
-                int diepte = 1 + std::max(diepteGebroken, diepteNietGebroken);
+                int diepte = 1 + std::max(diepteGebroken, diepteNietGebroken); // We willen het slechtste geval berekenen, dus het maximum van de twee
                 if(diepte < kostTabel[v][e]){
-                    kostTabel[v][e] = diepte;
-                    wortelTabel[v][e] = w;
+                    this->kostTabel[v][e] = diepte;
+                    this->wortelTabel[v][e] =  w;
                 }        
             }
         }
     }
 
-    printMatrix(kostTabel);
+    printMatrix(this->kostTabel);
     std::cout << "\n";
-    printMatrix(wortelTabel);
-    Boom<int, int> oplossingBoom;
-
-    int v = this->vGezocht;
-    int e = this->eGezocht;
-    int wortel = wortelTabel[v][e];
-    oplossingBoom.voegtoe(wortel, 0);
-
-    oplossingBoom.teken("eieren.dot");
-    return kostTabel[this->vGezocht][this->eGezocht];
+    printMatrix(this->wortelTabel);
 }
 
+
+int Eieren::geefGoedkoopsteOplossing() const {
+    return this->kostTabel[this->vGezocht][this->eGezocht];
+}
+
+
+Boom<int, int> Eieren::geefBoom() const {
+    Boom<int, int> boom;
+
+    int wortel = this->wortelTabel[this->vGezocht][this->eGezocht];
+    boom.voegtoe(wortel, 0);
+    
+    maakDeelbomen(boom, this->eGezocht, wortel - 1, this->vGezocht - wortel);
+
+  //  assert(boom.geefDiepte() == this->geefGoedkoopsteOplossing());
+    return boom;
+}
+
+
+void Eieren::maakDeelbomen(Boom<int, int>& wortel, int huidigAantalEieren, int aantalVerdiepingenLinks, int aantalVerdiepingenRechts) const {
+    if(aantalVerdiepingenLinks <= 0){
+        // dus een blad
+        return;
+    }
+
+    if(aantalVerdiepingenRechts <= 0){
+        // dus een blad
+        return;
+    }
+    std::cout << aantalVerdiepingenLinks << " - " << aantalVerdiepingenRechts << "\n";
+
+    // Stel dat het aantal verdiepingen links 3 is, dan zoeken we de beste wortel voor (1, 2, 3) met 1 ei minder
+    int wortelLinks = this->wortelTabel[aantalVerdiepingenLinks][huidigAantalEieren - 1];
+    (*wortel).links = std::make_unique<Knoop<int, int>>(wortelLinks, 0);
+    (*wortel).links->ouder = &(*wortel);
+    this->maakDeelbomen((*wortel).links, huidigAantalEieren - 1, wortelLinks - 1, (*wortel).sleutel - wortelLinks - 1);
+    
+    // Stel dat het aantal verdiepingen rechts 3 is, dan zoeken we de beste wortel voor ('wortel' + 1, 'wortel' + 2, 'wortel' + 3) met hetzelfde aantal eieren.
+    // Gelukkig komt dit neer op het zoeken naar de beste wortel voor (1, 2, 3) en daarbij dan 'wortel' bij op te tellen
+    int wortelRechts = this->wortelTabel[aantalVerdiepingenRechts][huidigAantalEieren] + (*wortel).sleutel;
+    (*wortel).rechts = std::make_unique<Knoop<int, int>>(wortelRechts, 0);
+    (*wortel).rechts->ouder = &(*wortel);
+    this->maakDeelbomen((*wortel).rechts, huidigAantalEieren, wortelRechts - (*wortel).sleutel - 1, this->vGezocht - wortelRechts);
+
+}
 
 #endif
--- Semester I/Gevorderde algoritmen/Labo 2019-2020/3a. Eieren/main.cpp
+++ Semester I/Gevorderde algoritmen/Labo 2019-2020/3a. Eieren/main.cpp
@@ -3,8 +3,10 @@
 
 
 int main(void){
-    Eieren eieren(8, 15);
-    std::cout << eieren.geefGoedkoopsteOplossing();
-
+    Eieren eieren(2, 5);
+    std::cout << "De goedkoopste oplossing bestaat uit " << eieren.geefGoedkoopsteOplossing()  << " pogingen.\n";  
+    Boom<int, int> boom = eieren.geefBoom();
+    boom.teken("eieren.dot");
+    boom.schrijf(std::cout);
     return 0;
 }
