be48ca20c4386b9f78a6344ce44f42e3110fa37e
monoscale <monoscale@outlook.com>
2019-12-08 14:46:37 +0100

    patriciatries

--- Semester I/Gevorderde algoritmen/Library/Eigen Library/strings/main.cpp
+++ Semester I/Gevorderde algoritmen/Library/Eigen Library/strings/main.cpp
@@ -1,12 +1,15 @@
 #include <string>
 #include <iostream>
 
-#include "patriciatrie.h"
+#include "patriciatrie.cpp"
 int main(void) {
     
     std::string ananas("ananas");
+    std::string anaconda("anaconda");
     PatriciaTrie trie;
+    
     trie.voegToe(ananas);
-
+    trie.voegToe(anaconda);
+    trie.teken("patriciatrie.dot");
     return 0;
 }
