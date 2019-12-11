78b43e3db9fe51de7e5144032ca92081b56dae27
unknown <monoscale@outlook.com>
2019-12-11 23:42:14 +0100

    gale shapley

--- Semester I/Gevorderde algoritmen/Library/Eigen Library/grafen/Gale-Shapley/GaleShapley-test.cpp
+++ Semester I/Gevorderde algoritmen/Library/Eigen Library/grafen/Gale-Shapley/GaleShapley-test.cpp
@@ -6,15 +6,18 @@ int main(void) {
     std::vector<std::vector<int>> m(4, std::vector<int>(4));
     std::vector<std::vector<int>> v(4, std::vector<int>(4));
 
-    m[0] = {0, 2, 1, 3};
-    m[1] = {2, 3, 0, 1};
-    m[2] = {3, 1, 2, 0};
-    m[3] = {2, 1, 0, 3};
 
-    v[0] = {1, 0, 2, 3};
-    v[1] = {3, 0, 1, 2};
-    v[2] = {0, 2, 1, 3};
-    v[3] = {1, 2, 0, 3};
+    m[0] = {1, 0, 2, 3};
+    m[1] = {3, 0, 1, 2};
+    m[2] = {0, 2, 1, 3};
+    m[3] = {1, 2, 0, 3};
+
+    v[0] = {0, 2, 1, 3};
+    v[1] = {2, 3, 0, 1};
+    v[2] = {3, 1, 2, 0};
+    v[3] = {2, 1, 0, 3};
+
+    
 
     GaleShapley gs(m, v);
     std::vector<int> koppeling = gs.koppel();
--- Semester I/Gevorderde algoritmen/Library/Eigen Library/grafen/Gale-Shapley/GaleShapley.h
+++ Semester I/Gevorderde algoritmen/Library/Eigen Library/grafen/Gale-Shapley/GaleShapley.h
@@ -15,42 +15,72 @@ private:
     std::vector<std::vector<int>> manVoorkeurslijst;
     std::vector<std::vector<int>> vrouwVoorkeurslijst;
 
-    int geefEersteVrij(std::vector<bool>& lijst) const;
     int GaleShapley::voorkeurVrouw(int vrouw, int man1, int man2) const;
 };
 
-int GaleShapley::geefEersteVrij(std::vector<bool>& lijst) const {
-    int i = 0;
-    while(lijst[i] == false){
-        i++;
-    }
-    
-    return i;
-}
 
 int GaleShapley::voorkeurVrouw(int vrouw, int man1, int man2) const {
-    int i = 0;
-    while(vrouwVoorkeurslijst[vrouw][i] != man1 || vrouwVoorkeurslijst[vrouw][i] != man2){
-        i++;
-    }
-    return i;
+    int voorkeur = -1;
+
+    std::vector<int>::const_iterator lijstIterator = vrouwVoorkeurslijst[vrouw].begin();
+    while(lijstIterator != vrouwVoorkeurslijst[vrouw].end()) {
+
+        // nagaan of huidige waarde één van de twee mannen is
+        if(*lijstIterator == man1){
+            voorkeur = man1;
+        } else if(*lijstIterator == man2){
+            voorkeur = man2;
+        }
+
+        if(voorkeur != -1){ // als de man gevonden werd
+            lijstIterator = vrouwVoorkeurslijst[vrouw].end() - 1; // iterator op einde zetten zodat while lus stopt, moet wel -1 want we doen op het einde nog eens iterator++
+        }
+        lijstIterator++;
+    } 
+
+    return voorkeur;
 }
 
-std::vector<int> GaleShapley::koppel() const {
-    std::vector<int> koppeling(manVoorkeurslijst.size(), -1);
 
-    std::vector<bool> manVrij(manVoorkeurslijst.size(), true);
-    std::vector<bool> vrouwVrij(vrouwVoorkeurslijst.size(), true);
-    std::vector<int> volgendeAanzoek(manVoorkeurslijst.size()); // deze vector bevat per man de index van de volgende vrouw in zijn voorkeurlijst die hij moet vragen
+/*
+algorithm stable_matching is
+    Initialize all m ∈ M and w ∈ W to free
+    while ∃ free man m who still has a woman w to propose to do
+        w = first woman on m's list to whom m has not yet proposed
+        if w is free then
+            (m, w) become engaged
+        else some pair (m', w) already exists
+            if w prefers m to m' then
+                m' becomes free
+                (m, w) become engaged 
+            else
+                (m', w) remain engaged
+            end if
+        end if
+    repeat
+*/
+std::vector<int> GaleShapley::koppel() const {
+    std::vector<int> koppeling(manVoorkeurslijst.size(), -1); // koppeling[i] geeft voor man i de vrouw die aan i gekoppeld werd
+
+    std::vector<bool> vrouwVrij(vrouwVoorkeurslijst.size(), true); // houdt bij welke vrouwen er nog vrij zijn
+
+    std::vector<int> volgendeAanzoek(manVoorkeurslijst.size()); // volgendeAanzoek[i] geeft voor man i de vrouw terug aan wie hij een aanzoek moet doen
     
-    int manIndex;
-    while((manIndex = geefEersteVrij(manVrij)) != manVrij.size()) {
+    std::queue<int> mannenZonderPartner;
+    for(int i = 0; i < manVoorkeurslijst.size(); i++){
+        mannenZonderPartner.emplace(i);
+    }
+    
+    while(!mannenZonderPartner.empty()){
+        int manIndex = mannenZonderPartner.front();
+        mannenZonderPartner.pop();
+
         int vrouwIndex = manVoorkeurslijst[manIndex][volgendeAanzoek[manIndex]];
-        volgendeAanzoek[manIndex]++;
+        volgendeAanzoek[manIndex]++; // de man moet zijn volgende aanzoek doen aan de vrouw op de volgende positie in zijn lijst
+
         if(vrouwVrij[vrouwIndex]) {
             // (man, vrouw) worden verloofd
             koppeling[manIndex] = vrouwIndex;
-            manVrij[manIndex] = false;
             vrouwVrij[vrouwIndex] = false;
         } else { // (man', vrouw) bestaat
             int andereManIndex = 0;
@@ -60,9 +90,11 @@ std::vector<int> GaleShapley::koppel() const {
             // als vrouw liever man heeft ipv man'
             if(voorkeurVrouw(vrouwIndex, manIndex, andereManIndex) == manIndex) {
                 koppeling[andereManIndex] = -1;
-                manVrij[andereManIndex] = true;
+                mannenZonderPartner.emplace(andereManIndex);
                 koppeling[manIndex] = vrouwIndex;
-                manVrij[manIndex] = false;
+                
+            } else {
+                mannenZonderPartner.emplace(manIndex);
             }
         }
     }
